cbd7cf8f6a88e4cf9a834558e734536fe8afbf31
preetimagadum <preetimagadum2@gmail.com>
2024-06-27 12:41:28 +0530

    java

---
+++ LinkedList/LinkedListOne.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class LinkedListOne{
+    public static void main(String[] args){
+        //finding size of linked lst
+        System.out.println(enames.size());
+        //varify the list is empty or not
+        System.out.println(enames.isempty());
+        
+
+    }
+}
---
+++ LinkedList/LinkedListTwo.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class LinkedListTwo{
+    public static void main(String[] args){
+        LinkedList<String> enames=new LinkedList<String>
+        System.out.println(enames);
+        //enames.add("rahul");
+    }
+}
