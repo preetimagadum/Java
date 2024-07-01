97a44e99f892ee3285a7010ba5844456799ff68a
preetimagadum <preetimagadum2@gmail.com>
2024-07-01 13:06:58 +0530

    task

---
+++ LinkedList/one.java
@@ -0,0 +1,12 @@
+import java.util.LinkedHashSet;
+
+class Test{
+    public static void main(String[] args){
+        LinkedHashSet<Integer> eids= new LinkedHashSet<Integer>();
+        eids.add(101);
+        eids.add(102);
+        eids.add(103);
+        eids.add(104);
+        eids.add(null);
+    }
+}
